c3006f7d37eb2e4560937376c2695910f76afb81
mplibunao <mark.libunao@shoresuite.com>
2017-11-26 18:52:52 +0800

    loading deck from file

--- deck.go
+++ deck.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
 	"strings"
 )
 
@@ -39,4 +41,19 @@ func deal(d deck, handSize int) (deck, deck) {
 // Make sense to tie it to the instance of this type
 func (d deck) toString() string {
 	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("err: ", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
 }
--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := newDeck()
-	fmt.Println(cards.toString())
+	// cards := newDeck()
+	// cards.saveToFile("deck")
+
+	cards := newDeckFromFile("deck1")
+	cards.print()
 }
