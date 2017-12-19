b61e65637b2eefd7f88890550fc060adcd4d0c10
mplibunao <mark.libunao@shoresuite.com>
2017-12-19 22:16:00 +0800

    added logging

--- routes.go
+++ routes.go
@@ -16,11 +16,16 @@ func NewRouter() *mux.Router {
 
 	router := mux.NewRouter().StrictSlash(true)
 	for _, route := range routes {
+		var handler http.Handler
+
+		handler = route.HandlerFunc
+		handler = Logger(handler, route.Name)
+
 		router.
 			Methods(route.Method).
 			Path(route.Pattern).
 			Name(route.Name).
-			Handler(route.HandlerFunc)
+			Handler(handler)
 	}
 
 	return router
@@ -32,6 +37,12 @@ var routes = Routes{
 		"GET",
 		"/",
 		Index,
+	},
+	Route{
+		"TodoIndex",
+		"GET",
+		"/todos",
+		TodoIndex,
 	},
 	Route{
 		"TodoShow",
