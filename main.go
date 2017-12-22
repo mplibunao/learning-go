57b438b87144e81890071fc5b91f83fa1d4fa866
mplibunao <mark.libunao@shoresuite.com>
2017-12-22 15:22:21 +0800

    created receivers for both english and spanish bot

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+type englishBot struct{}
+type spanishBot struct{}
+
 func main() {
 
+}
+
+func (englishBot) getGreeting() string {
+	// Very custom logic for generating an english greeting
+	return "Hi There!"
+}
+
+func (spanishBot) getGreeting() string {
+	return "Hola"
 }
