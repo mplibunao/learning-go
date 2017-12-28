98ca1e5cbeb50c7c460ed492c43196129a10c283
mplibunao <mark.libunao@shoresuite.com>
2017-12-28 11:28:16 +0800

    Added test tables

--- main_test.go
+++ main_test.go
@@ -10,3 +10,24 @@ func TestSum(t *testing.T) {
 		t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
 	}
 }
+
+// Test tables is a set(slice/array) of test input and output values
+func TestTableSum(t *testing.T) {
+	tables := []struct {
+		x int
+		y int
+		n int
+	}{
+		{1, 1, 2},
+		{1, 2, 3},
+		{2, 2, 4},
+		{5, 2, 7},
+	}
+
+	for _, table := range tables {
+		total := Sum(table.x, table.y)
+		if total != table.n {
+			t.Errorf("Sum of (%d+%d) was incorrect, got: %d, want: %d.", table.x, table.y, total, table.n)
+		}
+	}
+}
